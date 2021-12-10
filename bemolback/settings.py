51890fdd7cc0cb9ef7d47ae490a372946cff1638
Daniel Epichin <d.pena@hotmail.com>
2021-12-10 02:38:55 -0300

    feat: Implementando o heroku para deploy.

--- bemolback/settings.py
+++ bemolback/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +137,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+django_on_heroku.settings(locals())
